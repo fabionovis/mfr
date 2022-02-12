40051294cf3eec5ed4d9f1b2ec2d794e9a72b98c
f.novissimo <f.novissimo@studenti.unipi.it>
2022-02-12 17:17:10 +0100

    script improved

--- char_frequency.py
+++ char_frequency.py
@@ -5,7 +5,7 @@ import string
 
 logging.basicConfig(level=logging.INFO)
 
-def process(file_path):
+def process(file_path, hist=False, order=False):
     ''' This function does the main process
     '''
     assert file_path.endswith('.txt')
@@ -22,21 +22,36 @@ def process(file_path):
        character's frequency
     '''
     freq_dict = {key : 0 for key in string.ascii_lowercase}
-    
+   
     for ch in data.lower():
-        freq_dict[ch] += 1
+        if ch in string.ascii_lowercase:
+            freq_dict[ch] += 1
+            
+    if hist:
+        from matplotlib import pyplot as plt
+        if not order:
+            plt.bar(list(freq_dict.keys()), freq_dict.values())
+        else:
+            sorted_freq_dict = {k:v for k, v in sorted(freq_dict.items(), key = lambda item: item[1])}
+            plt.bar(list(sorted_freq_dict.keys()), sorted_freq_dict.values(), log = True)
+        plt.show()
         
     norm = float(sum(freq_dict.values()))
     
-    for ch in string_ascii_lowercase:
+    for ch in string.ascii_lowercase:
         freq_dict[ch]/=norm
         
     for ch, freq in freq_dict.items():
         print('{}: {:.3f}%'.format(ch, freq * 100.))
+        
+    
          
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', help = 'path to the input file')
-    
+    parser.add_argument('--histo', help = 'Plot the histogram of the relative frequency of each character', action = 'store_true')
+    parser.add_argument('--order', help = 'Plot the histogram with the entries in increasing order', action = 'store_true')
+        
     args = parser.parse_args()
-    process(args.infile)
+    process(args.infile, args.histo, args.order)
+    
